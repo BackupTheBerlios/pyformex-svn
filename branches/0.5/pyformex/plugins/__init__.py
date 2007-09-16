b5cf5b6553f54fdc8637cb6434884b5c3dabb60c
bverheg <bverheg@8d6f1305-3bde-0310-9e88-884b4813ce35>
2007-09-16 20:13:02 +0000

    Branch with the original Formex implementation

---
+++ branches/0.5/pyformex/plugins/__init__.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# $Id$
+##
+## This file is part of pyFormex 0.5 Release Fri Aug 10 12:04:07 2007
+## pyFormex is a Python implementation of Formex algebra
+## Website: http://pyformex.berlios.de/
+## Copyright (C) Benedict Verhegghe (benedict.verhegghe@ugent.be) 
+##
+## This program is distributed under the GNU General Public License
+## version 2 or later (see file COPYING for details)
+##
+"""pyFormex plugins initialisation.
+
+Currently, this does nothing. The file should be kept though, because it is
+needed to flag this directory as a Python module.
+"""
